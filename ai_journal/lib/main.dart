f6378fd1200d7ce1dbcda55f59e2a70d656ec06e
jingyima <jinma717@gmail.com>
2024-12-11 15:39:55 -0500

    First version

--- ai_journal/lib/main.dart
+++ ai_journal/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +8,175 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Journal App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
         useMaterial3: true,
+        fontFamily: 'Courier New',
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const JournalEntryPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class JournalEntryPage extends StatefulWidget {
+  const JournalEntryPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<JournalEntryPage> createState() => _JournalEntryPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _JournalEntryPageState extends State<JournalEntryPage> {
+  final TextEditingController _journalController = TextEditingController();
+  String? _aiResponse;
+  String _currentDisplayedResponse = '';
+  Timer? _timer;
+  bool _isTyping = false;
 
-  void _incrementCounter() {
+  @override
+  void dispose() {
+    _journalController.dispose();
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  void _startTypingEffect(String text) {
+    if (_isTyping) return;
+    
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _isTyping = true;
+      _currentDisplayedResponse = '';
+      _aiResponse = text;
+    });
+
+    int index = 0;
+    _timer?.cancel();
+    
+    _timer = Timer.periodic(const Duration(milliseconds: 50), (timer) {
+      if (index < text.length) {
+        setState(() {
+          _currentDisplayedResponse = text.substring(0, index + 1);
+          index++;
+        });
+      } else {
+        _isTyping = false;
+        timer.cancel();
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: const Color(0xFFF3F2EE),
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: const Color(0xFFF3F2EE),
+        elevation: 0,
+        leading: const BackButton(color: Colors.black),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Friday, December 11 2024 at 9:00am',
+              style: TextStyle(
+                fontFamily: 'Courier New',
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black.withOpacity(0.8),
+              ),
+            ),
+            const SizedBox(height: 8),
+            Row(
+              children: [
+                Icon(Icons.cloud, size: 16, color: Colors.grey[600]),
+                const SizedBox(width: 4),
+                Text(
+                  'new york city, ny',
+                  style: TextStyle(
+                    fontFamily: 'Courier New',
+                    fontSize: 14,
+                    color: Colors.grey[600],
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+            TextField(
+              controller: _journalController,
+              maxLines: null,
+              decoration: const InputDecoration(
+                hintText: 'Write your thoughts...',
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.zero,
+              ),
+              style: TextStyle(
+                fontFamily: 'Courier New',
+                fontSize: 16,
+                color: Colors.black.withOpacity(0.8),
+                height: 1.5,
+              ),
+            ),
+            const SizedBox(height: 24),
+            if (_aiResponse != null)
+              Container(
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: const Color(0xFFF3F2EE),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Text(
+                  _currentDisplayedResponse,
+                  style: const TextStyle(
+                    fontFamily: 'Courier New',
+                    fontSize: 15,
+                    color: Color(0xFFCB984F),
+                    height: 1.5,
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: const BoxDecoration(
+          color: Color(0xFFF3F2EE),
+          border: Border(top: BorderSide(color: Colors.black12)),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              icon: Icon(Icons.mic, color: Colors.grey[600]),
+              onPressed: () {
+                // TODO: Implement voice input
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.star, color: Colors.grey[600]),
+              onPressed: () {
+                _startTypingEffect(
+                  "I hear you, and it sounds like you're going through a challenging time. "
+                  "Remember that test scores don't define your worth. You're capable of so much more "
+                  "than you realize, and setbacks are just temporary obstacles on your path to success."
+                );
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.check, color: Colors.grey[600]),
+              onPressed: () {
+                // Left empty for now since we're using the star button
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
